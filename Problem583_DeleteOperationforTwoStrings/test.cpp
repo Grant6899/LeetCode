5e6879500018e833639c4e5c5857d44c31a96e87
刘一铭 <ymliu6899@gmail.com>
2017-12-12 20:35:58 -0500

    Problem583 change index

--- Problem583_DeleteOperationforTwoStrings/test.cpp
+++ Problem583_DeleteOperationforTwoStrings/test.cpp
@@ -18,7 +18,7 @@ public:
         
         for (int i = 1; i <= len1; ++i)
         for (int j = 1; j <= len2; ++j){
-            if (word1[i] == word2[j])
+            if (word1[i - 1] == word2[j - 1])
                 dp[i][j] = dp[i-1][j-1] + 1;
             else
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);

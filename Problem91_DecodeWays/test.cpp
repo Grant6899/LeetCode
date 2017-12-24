bd488c60aa84285a897b6de1de67d7e55284d4ac
刘一铭 <ymliu6899@gmail.com>
2017-12-24 15:14:27 -0500

    91 fix

--- Problem91_DecodeWays/test.cpp
+++ Problem91_DecodeWays/test.cpp
@@ -13,17 +13,34 @@ class Solution {
 public:
     int numDecodings(string s) {
         vector<int> dp(s.size() + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= s.size(); ++i){
-            if(s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6')){
-                dp[i] = dp[i-1] + dp[i-2];
-            }
-            else
-                dp[i] = dp[i-1];
-        }
+        if(s.size() == 0 || s[0] == '0')
+            return 0;
         
+        if(s[0] != '0')
+            return 1;
+
+        dp[0] = dp[1] = 1;
+
+        for(int i = 2; i <= s.size(); ++i){
+            if( valid(s[i-1]) && valid(s[i-2], s[i-1]) )
+                dp[i] = dp[i-1] + dp[i-2];
+            if( !valid(s[i-1]) && valid(s[i-2], s[i-1]) )
+                dp[i] = dp[i-2];
+            if( valid(s[i-1]) && !valid(s[i-2], s[i-1]) )
+                dp[i] = dp[i-1];
+            if( !valid(s[i-1]) && !valid(s[i-2], s[i-1]) )
+                return 0;
+        }
         return dp[s.size()];
+    }
+
+private:
+    bool valid(char ch1, char ch2){
+        return (ch1 == '1' || (ch1 == '2' && ch2 <= '6'));
+    }
+
+    bool valid(char ch){
+        return ch != '0';
     }
 };
 

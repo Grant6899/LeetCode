3f9ad13b95da61449eebd6deba3e6e4b8ca0ac35
刘一铭 <ymliu6899@gmail.com>
2017-12-24 15:39:57 -0500

    init 93

---
+++ Problem93_RestoreIPAddresses/test.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: Grant Liu
+	> Mail: ymliu6899@gmail.com
+	> Created Time: Sun Dec 24 15:19:41 2017
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> result;
+        vector<int> current;
+        find(0, 0, result, current, s);
+        return result;
+    }
+
+    void find(int next, int cur_loc, vector<string>& result, vector<int>& current, const string& s){
+        if(next == 4 && cur_loc == s.size()){
+            result.push_back(currenttostr(current));
+            return;
+        }
+        
+        for(int i = cur_loc + 1; i <= std::min(cur_loc + 3, (int)(s.size())); ++i){
+            if( s[i] != '0' && stoi(s.substr(cur_loc, i)) <= 255 ){
+                current.push_back( stoi(s.substr(cur_loc, i)) );
+                find(next + 1, i, result, current, s);
+                current.pop_back();
+            }
+        }
+    }
+
+    string currenttostr(const vector<int>& cur){
+        string s = to_string(cur[0]);
+        for(int i = 1; i <= 3; ++i)
+            s += "." + to_string(cur[i]);
+        return s;
+    }
+
+};
+
+int main(){
+    Solution s;
+    auto ans = s.restoreIpAddresses("25525511135");
+    for(auto it = ans.begin(); it < ans.end(); ++it)
+        cout << *it << "|";
+    cout << endl;
+    return 0;
+}
+

14b3865914498916e8c77ea59b25c97010401dac
刘一铭 <ymliu6899@gmail.com>
2017-12-24 16:08:55 -0500

    init 93

--- Problem93_RestoreIPAddresses/test.cpp
+++ Problem93_RestoreIPAddresses/test.cpp
@@ -20,24 +20,37 @@ public:
         return result;
     }
 
-    void find(int next, int cur_loc, vector<string>& result, vector<int>& current, const string& s){
-        if(next == 4 && cur_loc == s.size()){
+    void find(int next, int cur_loc, vector<string>& result, vector<int>& current, const string& s) {
+        if (next == 4 && cur_loc == s.size()) {
             result.push_back(currenttostr(current));
             return;
         }
-        
-        for(int i = cur_loc + 1; i <= std::min(cur_loc + 3, (int)(s.size())); ++i){
-            if( s[i] != '0' && stoi(s.substr(cur_loc, i)) <= 255 ){
-                current.push_back( stoi(s.substr(cur_loc, i)) );
-                find(next + 1, i, result, current, s);
-                current.pop_back();
+
+
+        if (s[cur_loc] == '0') {
+            current.push_back(0);
+            find(next + 1, cur_loc + 1, result, current, s);
+            current.pop_back();
+        }
+        else {
+            for (int i = 1; i <= 3 && cur_loc + i <= s.size(); ++i) {
+                int temp = stoi(s.substr(cur_loc, i));
+                if (((s.size() - cur_loc - i) >= (3 - next))
+                    && ((s.size() - cur_loc - i) <= (3 - next) * 3)
+                    && temp <= 255
+                    ) {
+                    current.push_back(stoi(s.substr(cur_loc, i)));
+                    find(next + 1, cur_loc + i, result, current, s);
+                    current.pop_back();
+                }
             }
         }
+        
     }
 
-    string currenttostr(const vector<int>& cur){
+    string currenttostr(const vector<int>& cur) {
         string s = to_string(cur[0]);
-        for(int i = 1; i <= 3; ++i)
+        for (int i = 1; i <= 3; ++i)
             s += "." + to_string(cur[i]);
         return s;
     }

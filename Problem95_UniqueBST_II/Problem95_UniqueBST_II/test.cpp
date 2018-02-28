484adfc52948769e0aab9f552271348b9cbfdb4a
刘一铭 <ymliu6899@gmail.com>
2018-02-27 19:59:22 -0500

    Good Luck

--- Problem95_UniqueBST_II/Problem95_UniqueBST_II/test.cpp
+++ Problem95_UniqueBST_II/Problem95_UniqueBST_II/test.cpp
@@ -1,9 +1,5 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-#include<cstdlib>
-#include<string>
-#include<unordered_map>
 
 using namespace std;
 
@@ -16,65 +12,53 @@ struct TreeNode {
 };
 
 class Solution {
-private:
-	TreeNode* q;
-	vector < vector<TreeNode*>> g{ {NULL}, { q = new TreeNode(1)} };
 public:
-	vector<TreeNode*> generateTrees(int n) {
-		if (n < 2)
-			return g[n];
-		else {
-			for (int i = 2; i <= n; i++) {
-				vector<TreeNode*> result;
-				for (int j = 1; j <= i; j++) {
-					
-					for (auto left_it = g[j-1].begin(); left_it < g[j-1].end(); left_it++)
-						for (auto right_it = g[i - j].begin(); right_it < g[i - j].end(); right_it++) {
-							TreeNode* current_root = new TreeNode(j);
-							current_root->left = *left_it;
-							TreeNode* current_right = new TreeNode(*right_it);
-							current_root->left = *left_it;
+    vector<TreeNode*> generateTrees(int n) {
+        vector<vector<TreeNode*>> res;
+        
+        if(n == 0)
+            return vector<TreeNode*>();
+        
+        res.push_back({nullptr});
+        res.push_back({new TreeNode(1)});
+        
+        for(int i = 2; i <= n; ++i){
+            vector<TreeNode*> cur;
+            
+            for(int mid = 1; mid <= i; ++mid){
 
-						}
+                for(TreeNode* left : res[mid - 1])
+                for(TreeNode* right : res[i - mid]){
+                    TreeNode* new_root = new TreeNode(mid);
+                    
+                    new_root->left = left;
+                    
+                    new_root->right = copyTree(right, mid);
+                    
+                    cur.push_back(new_root);
+                }
+            }
+            res.push_back(cur);
+        }
+            
+        return res[n];        
+    }
+    
+    TreeNode* copyTree(TreeNode* root, int diff){
+        if(!root)
+            return nullptr;
+        
+        TreeNode* new_root = new TreeNode(root->val + diff);
+        new_root->left = copyTree(root->left, diff);
+        new_root->right = copyTree(root->right, diff);
+        return new_root;
+    }
 
-				}
-			}
-
-
-		}
-
-	}
 };
-
-
-TreeNode* createTree(const vector<int>& src, int i) {
-	if (src[i] == -1)
-		return NULL;
-
-	TreeNode* current = new TreeNode(src[i]);
-	if (i * 2 + 1 < src.size())
-		current->left = createTree(src, i * 2 + 1);
-	else
-		current->left = NULL;
-
-	if (i * 2 + 2 < src.size())
-		current->right = createTree(src, i * 2 + 2);
-	else
-		current->right = NULL;
-	return current;
-}
-
 
 int main() {
 	Solution s;
-
-
-
-	vector<int> src = { 1,2,3,4,5,6,7,8,-1,-1,9 };
-
-	TreeNode* root = createTree(src, 0);
-
-	auto ans = s.numTrees(4);
+	auto ans = s.generateTrees(3);
 
 	return 0;
 }
